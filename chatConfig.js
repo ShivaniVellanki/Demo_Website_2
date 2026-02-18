038de6c8a2eaebc9238226be79164c763fecbde4
ShivaniVellanki <139008609+ShivaniVellanki@users.noreply.github.com>
2026-02-18 14:47:42 -0500

    Update chatConfig.js

--- chatConfig.js
+++ chatConfig.js
@@ -6,7 +6,7 @@ let botOptions=chatConfig.botOptions;
 
 userIdentity = crypto.randomUUID();
 
-botOptions.JWTUrl = "https://epson-jwt-multi.vercel.app/api/sts?bot=na-customer";
+botOptions.JWTUrl = "https://epson-jwt-multi.vercel.app/api/sts?bot=la-customer";
  botOptions.userIdentity = userIdentity;
  botOptions.botInfo = { name: "Prod Epson Test", "_id": "st-a262f3ad-040f-5b60-8d57-87abcc2fa38a" };
  botOptions.clientId = "cs-79ae2a3d-931c-59c3-a3f2-b5c49a3a5b7f";
